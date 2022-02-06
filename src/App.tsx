36e8ee407375849897c219ac0e99853c597e07b5
Davi F <davi.felix.lima@gmail.com>
2022-02-06 14:20:45 -0300

    create Content component

--- src/App.tsx
+++ src/App.tsx
@@ -58,19 +58,10 @@ export function App() {
         handleClickButton={handleClickButton}
       />
 
-    <div className="container">
-      <header>
-        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-      </header>
-
-      <main>
-        <div className="movies-list">
-          {movies.map(movie => (
-            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-          ))}
-        </div>
-      </main>
-    </div>
+      <Content 
+        selectedGenre={selectedGenre}
+        movies={movies}
+      />
       
     </div>
   )
